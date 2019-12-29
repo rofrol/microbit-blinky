f13b1d8e787fd1530de8a134b5ba6c1cce5ea8c0
Roman Frołow <rofrol@gmail.com>
2019-12-29 18:34:53 +0100

    loop

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 #![no_main]
 
 extern crate panic_halt;
+extern crate microbit;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
     loop {}
 }
